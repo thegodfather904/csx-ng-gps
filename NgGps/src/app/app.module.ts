b33369d3f60954e0fe0c4812e21b7353bc1026f6
thegodfather904 <arscialo@gmail.com>
2018-02-21 14:20:22 -0500

    material module added

--- NgGps/src/app/app.module.ts
+++ NgGps/src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
